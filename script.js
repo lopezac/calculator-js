961059b53250237fee0d65dc935f065f5b08d25c
Axel Lopez <lopezaxel@protonmail.com>
2022-05-16 10:31:18 -0300

    Add addToDisplay, formatCalculation, isNum and selectInputOperation functions

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll(".btn");
+
+buttons.forEach(btn => btn.addEventListener("click", (e) => {
+    selectInputOperation(e.target.textContent)
+}));
+
+function selectInputOperation(btn) {
+    if (btn === "=") {
+        doCalculation();
+    } else if (btn === "Clear") {
+        console.log("Do clear operation");
+    } else if (btn === "Undo") {
+        console.log("Do undo operation");
+    } else {
+        addToDisplay(btn);
+    }
+}
+
+function doCalculation() {
+    let calculation = formatCalculation();
+
+    return calculation;
+}
+
+function addToDisplay(btn) {
+    display.textContent += btn;
+}
+
+function formatCalculation() {
+    let formattedCalc = display.textContent.split("");
+    for (let i = 0; i < formattedCalc.length; i++) {
+        if (isNum(formattedCalc[i]) && isNum(formattedCalc[i + 1])) {
+            formattedCalc[i + 1] += formattedCalc[i];
+            formattedCalc.splice(i, 1);
+        }
+    }
+    return formattedCalc;
+}
+
+function isNum(string) {
+    return !isNaN(string);
+}
+
 function add(num1, num2) {
     return Number(num1) + Number(num2);
 }
