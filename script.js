34cd6472502384951455756ffbb86615a96839f9
Axel Lopez <lopezaxel@protonmail.com>
2022-05-17 10:59:42 -0300

    Fix error with formatting zeros

--- script.js
+++ script.js
@@ -31,7 +31,11 @@ function doCalculation() {
 function doOperatorsCalc(calc, operators) {
     let i = 1;
     while (i < calc.length) {
-        calc[i] = doOperation(calc, operators, i);   
+        calc[i] = doOperation(calc, operators, i);
+        // if (isDecimal(calc[i])) {
+        //     let decimalLength = calc[i].toString().slice(calc[i].indexOf(".") + 1, calc.length).length;
+        //     if (decimalLength > 2) calc[i] = calc[i].toFixed(2);
+        // }
 
         if (isNum(calc[i])) {
             calc.splice(i - 1, 1);
@@ -73,21 +77,57 @@ function updateDisplay(text) {
 }
 
 function formatCalculation() {
-    let formattedCalc = display.textContent.split("");
+    let fCalc = display.textContent.split("");
     let i = 0;
-    while (i < formattedCalc.length) {
-        if (isNum(formattedCalc[i]) && isNum(formattedCalc[i + 1])) {
-            formattedCalc[i + 1] = formattedCalc[i] + formattedCalc[i + 1];
-            formattedCalc.splice(i, 1);
+    while (i < fCalc.length) {
+        if (isNum(fCalc[i])) {
+            
+            if (isNum(fCalc[i + 1])) {
+                // let nextOperator = getNextOperator(fCalc, i);
+                if(fCalc[i + 1] === "0") {
+                    fCalc[i + 1] = fCalc[i].toString() + fCalc[i + 1].toString();
+                } else {
+                    fCalc[i + 1] = fCalc[i] + fCalc[i + 1];
+                }
+                fCalc.splice(i, 1);
+                continue;
+            }
+
+            if (isSumOperator(fCalc[i - 1]) && 
+            ((fCalc[i - 2] === undefined) || isOperator(fCalc[i - 2]))) {
+                fCalc[i] = fCalc[i - 1] + fCalc[i];
+                fCalc.splice(i - 1, 1);
+            } else {
+                i += 1;
+            }
         } else {
             i += 1;
         }
+
     }
-    return formattedCalc;
+    return fCalc;
+}
+
+function getNextOperator(calc, numIndex) {
+    for (let i = numIndex; i < calc.length; i++) {
+        if (isOperator(calc[i])) return i;
+    }
 }
 
 function isNum(string) {
     return !isNaN(string);
+}
+
+function isOperator(string) {
+    return ["*", "/", "+", "-"].includes(string);
+}
+
+function isDecimal(num) {
+    return num.indexOf("*") !== "-1";
+}
+
+function isSumOperator(string) {
+    return ["+", "-"].includes(string);
 }
 
 function add(num1, num2) {
@@ -95,7 +135,7 @@ function add(num1, num2) {
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
+    return Number(num1) - Number(num2);
 }
 
 function multiply(num1, num2) {
